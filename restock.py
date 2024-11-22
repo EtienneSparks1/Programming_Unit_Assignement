357fbe6170f03abe15d24f1bfc75a76a0bed9ff4
Etienne Sparks <etiennesparks@gmail.com>
2024-11-22 04:08:36 +0000

    Completed restock_inventory and daily_sales functions

--- restock.py
+++ restock.py
@@ -18,4 +18,11 @@ available_items:(integer) This function returns this integer which updates the a
 The function will also update the inventory_records (For restocking) for a  given current day. It will also return "available_items".
     '''
 
+    if current_day == 0:
+        available_items = 2000 
+        inventory_tuple = (current_day, 0, 2000, 2000) # Sets a base for the stock of units at the start
+        inventory_records.append(inventory_tuple) # Adds inventory_tuple to the list of inventory records
+    elif current_day % 7 == 0: # If the current day is a Sunday
+        inventory_tuple = (current_day, 0, 2000 - available_items, 2000) # adds a row in the list for each restock at the end of the week, calculates what needs to be restocked based on sales
+        inventory_records.append(inventory_tuple) # adds inventory_tuple to the inventory report
     return available_items
--- sales.py
+++ sales.py
@@ -18,6 +18,10 @@ available_items:(integer) This function returns this integer which updates the a
 
 The function will also update the inventory_records (For restocking) for a  given current day. 
 
-    '''
-    
+    ''' 
+    if current_day % 7 != 0 and available_items != None: # If it is not the end of the week and there is still units available
+        sales = random.randint(1, 200) # Generates a random amount of sales for each day
+        available_items = available_items - sales # Decreases the amount of stock according to the number of sales made
+        inventory_tuple = (current_day, sales, 0, available_items)
+        inventory_records.append(inventory_tuple) # Adds the inventory sales 
     return available_items
